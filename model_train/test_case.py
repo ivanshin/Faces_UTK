8b78ed5694f42b63e2b14316a5e9d877c88e999e
ivanshin <i.shingel@outlook.com>
2022-06-13 15:59:16 +0300

    age gender race model created, bad results

--- model_train/test_case.py
+++ model_train/test_case.py
@@ -9,11 +9,11 @@ from PIL import Image
 print(os.curdir)
 
 # model load
-cnn_model_pretrained = tf.keras.models.load_model(os.path.join('.', 'model_train', 'trained_model', 'cnn_v2'), custom_objects=None, compile=True, options=None)
+cnn_model_pretrained = tf.keras.models.load_model(os.path.join('.', 'model_train', 'trained_model', 'cnn_v3'), custom_objects=None, compile=True, options=None)
 cnn_model_pretrained.summary()
 
 # load test image
-test_img = Image.open(os.path.join('.', 'model_train', 'test.jpg'))
+test_img = Image.open(os.path.join('.', 'model_train', 'test_2.jpg'))
 test_img.show()
 
 # resize image
@@ -28,7 +28,7 @@ preds = cnn_model_pretrained.predict(test)
 
 # show res
 plt.imshow(test_img)
-y_resh = tf.reshape(preds[0],[68, 2])
-plt.scatter(*zip(*y_resh), marker= 'x', color= 'b')
-plt.show()
+preds = np.around(preds)
+plt.title(f"age: {round(preds[0][0])}, gender: {preds[0][1]}, race: {preds[0][2]}")
+
 
