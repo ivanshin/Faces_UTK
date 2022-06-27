587f099d61172082520b297c78464bb6f88fd774
ivanshin <i.shingel@outlook.com>
2022-06-27 16:37:51 +0300

    age model ready with test case

--- api/client_test.py
+++ api/client_test.py
@@ -6,7 +6,7 @@ import requests
 import matplotlib.pyplot as plt
 
 
-FILEPATH = './model_train/test_2.jpg'
+FILEPATH = './model_train/test3.jpg'
 
 # read image as file
 img = open( FILEPATH , 'rb')
@@ -14,12 +14,13 @@ files = {'file': ('test_image', img, "image/jpeg")}
 
 # send request
 resp = requests.post(" http://127.0.0.1:8000/api/predictions", files= files)
-print(resp.json())
+print(resp.json())  
 
 
 # illustrate results
 resp = json.loads(resp.json())
 pil_img = Image.open(FILEPATH)
 plt.imshow(pil_img)
-plt.title(f"AGE: {resp['age']}, GENDER {resp['gender']}, RACE: {resp['race']}")
+#plt.title(f"AGE: {resp['age']}, GENDER {resp['gender']}, RACE: {resp['race']}")
+plt.title(f"AGE: {resp['age']}")
 plt.show()
--- api/server.py
+++ api/server.py
@@ -12,7 +12,7 @@ import io
 
 # create app and load model
 service = FastAPI()
-model = tf.keras.models.load_model(r'D:\Projects\Python\Faces_UTK\model_train\trained_model\cnn_v3')
+model = tf.keras.models.load_model(r'D:\Projects\Python\Faces_UTK\model_train\trained_model\age_model.h5')
 
 @service.get("/")
 async def read_root():
@@ -37,21 +37,22 @@ async def grab_image(file: bytes= File()):
         
         Example request:
         resp = requests.post(" http://127.0.0.1:8000/api/predictions", files= files)
-    """
+    """ 
+
 
     image = np.array(Image.open(io.BytesIO(file)))
-    image = image / 255
-    image = np.resize(image, (200, 200, 3))
-    image = image.reshape((1, 200, 200,3))
-    #return f'shape: {image.shape}'
+    image = tf.image.resize(image, [224,224]) 
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = image / 255.0      
+    image = tf.expand_dims(image, axis=0)
 
     predictions = model.predict(image)
     
     predictions = np.around(predictions)
     data = {}
     data['age'] = str(predictions[0][0])
-    data['gender'] = str(predictions[0][1])
-    data['race'] = str(predictions[0][2])
+    #data['gender'] = str(predictions[0][1])
+    #data['race'] = str(predictions[0][2])
     predictions_json = json.dumps(data)
 
     return predictions_json
