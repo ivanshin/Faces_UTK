ec5d3f62448937d7a8698098bdce97c9fcc1a2bf
DmitriyKovchugo <dkovchugo@mail.ru>
2022-06-28 16:20:57 +0300

    feat: drag & drop markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
-import './App.css';
+import classes from './App.module.css';
+import React, {useState} from "react";
 
 function App() {
+  const [isDrag, setDrag] = useState(false)
+
+  function dragStartHandler(e) {
+    e.preventDefault()
+    setDrag(true)
+  }
+
+  function dragLeaveHandler(e) {
+    e.preventDefault()
+    setDrag(false)
+  }
+
+  function onDropHandler(e) {
+    e.preventDefault()
+    let files = [...e.dataTransfer.files]
+    setDrag(false)
+    console.log(files)
+  }
+
   return (
-    <div className="App">
+    <div className={classes.App}>
+      {isDrag
+        ? <div
+            onDragStart={(e) => dragStartHandler(e)}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragOver={(e) => dragStartHandler(e)}
+            onDrop={e => onDropHandler(e)}
+          >
+            Отпустите изображение, чтобы загрузить его
+      </div>
+        : <div
+            onDragStart={(e) => dragStartHandler(e)}
+            onDragLeave={(e) => dragLeaveHandler(e)}
+            onDragOver={(e) => dragStartHandler(e)}
+          >Перетащите изображение, чтобы загрузить его</div>
+      }
 
     </div>
   );
