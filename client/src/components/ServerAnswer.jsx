13cc24f2fa9bb815f77b435d167babcd9f6007ed
ivanshin <i.shingel@outlook.com>
2022-08-23 11:47:08 +0300

    b_boks ready

--- client/src/components/ServerAnswer.jsx
+++ client/src/components/ServerAnswer.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const ServerAnswer = (props) => {
-    {console.log(props.answer['b_box'])}
+    {console.log(props.answer)}
     return (
         <div className={"serverAnswer__container"}>
             <div className="image-container">
                 <img src={props.photoURL} alt="UserPhoto"/>
             </div>
-            <div className='b-box'>
-
+            <div className='b-box' style={{width:`${props.answer['width']}px`, height:`${props.answer['height']}px`, left:`${props.answer['left']}px`,top:`${props.answer['top']}px`}}>
             </div>
             <div className="answer">
                 <div className="answer__age">
