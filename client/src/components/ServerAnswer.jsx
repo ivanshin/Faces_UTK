43b5fed078f4ed3a4a7eca6180ae5b48157615b5
ivanshin <i.shingel@outlook.com>
2022-08-23 10:52:25 +0300

    b_box coords added

--- client/src/components/ServerAnswer.jsx
+++ client/src/components/ServerAnswer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const ServerAnswer = (props) => {
+    {console.log(props.answer['b_box'])}
     return (
         <div className={"serverAnswer__container"}>
             <div className="image-container">
                 <img src={props.photoURL} alt="UserPhoto"/>
+            </div>
+            <div className='b-box'>
+
             </div>
             <div className="answer">
                 <div className="answer__age">
