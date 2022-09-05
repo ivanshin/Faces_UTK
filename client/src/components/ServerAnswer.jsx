05b34413f851c92798e6d9ca54e3f76362cc3dee
DmitriyKovchugo <dkovchugo@mail.ru>
2022-09-05 14:19:30 +0300

    feat: fixed window size && start work with currentWidth

--- client/src/components/ServerAnswer.jsx
+++ client/src/components/ServerAnswer.jsx
@@ -13,16 +13,16 @@ const ServerAnswer = () => {
                 <img ref={userPhoto} src={photoURL} alt="UserPhoto"/>
             </div>
             {console.log(userPhoto)}
-            {/*<div className='b-box' style={{width:`${answer['width']}px`, height:`${answer['height']}px`, left:`${answer['left']}px`,top:`${answer['top']}px`}}>*/}
-            {/*</div>*/}
-            {/*<div className="answer">*/}
-            {/*    <div className="answer__age">*/}
-            {/*        <span>Age: </span>{Math.round(answer['age'])}*/}
-            {/*    </div>*/}
-            {/*    <div className="answer__gender">*/}
-            {/*        <span>Gender: </span>{answer['gender']}*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className='b-box' style={{width:`${answer['width']}px`, height:`${answer['height']}px`, left:`${answer['left']}px`,top:`${answer['top']}px`}}>
+            </div>
+            <div className="answer">
+                <div className="answer__age">
+                    <span>Age: </span>{Math.round(answer['age'])}
+                </div>
+                <div className="answer__gender">
+                    <span>Gender: </span>{answer['gender']}
+                </div>
+            </div>
         </div>
     );
 };
